3f4ad764dd3854834100543f74b317fcc739b97c
GalaxyShard <galaxyshard.tech@proton.me>
2024-02-03 13:59:58 -0500

    changed editor styling & toolbar

--- editor.js
+++ editor.js
@@ -30,48 +30,77 @@ function removeHoverToolbar(para) {
         buttons[0].remove();
     }
 }
+
 function createEditorBox(para) {
     let container = document.createElement("div");
     container.classList.add("editor-container");
 
-    let editor = document.createElement("textarea");
+    let editor = document.createElement("div");
     editor.classList.add("editor");
+    editor.contentEditable = "true";
+    editor.addEventListener("beforeinput", e => {
+        // e.preventDefault();
+    });
+    editor.addEventListener("input", e => {
+        // console.log(editor.value);
+        // editor.innerHTML = editor.innerHTML.replace(/(\n|<br>){2,}/g, "\n");
+    });
+    // editor.addEventListener("paste", e => {
+    //     console.log("paste, ", e);
+    // });
 
 
 
     let toolbar = document.createElement("div");
     toolbar.classList.add("editor-toolbar");
 
-    // consider using a markdown-like format instead of buttons?
     let bold = document.createElement("button");
     bold.classList.add("editor-bold");
     bold.textContent = "Bold";
+    bold.addEventListener("click", e => {
 
-    let strong = document.createElement("button");
-    strong.classList.add("editor-strong");
-    strong.textContent = "Emphasis";
+    });
 
     let italic = document.createElement("button");
     italic.classList.add("editor-italic");
     italic.textContent = "Italic";
+    italic.addEventListener("click", e => {
 
-    let em = document.createElement("button");
-    em.classList.add("editor-em");
-    em.textContent = "Emphasis";
+    });
+
+    let link = document.createElement("button");
+    link.classList.add("editor-link");
+    link.textContent = "Link";
+    link.addEventListener("click", e => {
+
+    });
+
+    let cancel = document.createElement("button");
+    cancel.classList.add("editor-cancel");
+    cancel.textContent = "Cancel";
+    cancel.addEventListener("click", e => {
+        let para = document.createElement("p");
+        para.innerHTML = container.dataset.original;
+        container.parentElement.replaceChild(para, container);
+    });
 
     let save = document.createElement("button");
     save.classList.add("editor-save");
     save.textContent = "Save";
+    save.addEventListener("click", e => {
+
+    });
 
 
 
     removeHoverToolbar(para);
+    container.dataset.original = para.innerHTML;
     let text = para.innerHTML.trim();
-    
-    
-    editor.textContent = text;
+    text = text.replace(/(\n|<br>){2,}/g, "\n");
 
-    toolbar.append(bold, strong, italic, em, save);
+    editor.innerHTML = text;
+
+    toolbar.append(bold, italic, link, cancel, save);
     container.append(editor, toolbar);
     para.parentElement.replaceChild(container, para);
 }
